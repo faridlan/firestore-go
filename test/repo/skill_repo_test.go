88d26ee0b53d094393ae31574651b480bb1a11e3
Faridlan Nul Hakim <faridlannulhakim@gmail.com>
2024-07-01 23:49:54 +0700

    [ADD] try to add unit test

--- test/repo/skill_repo_test.go
+++ test/repo/skill_repo_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/faridlan/firestore-go/config"
+	"github.com/faridlan/firestore-go/model/domain"
 	skillrepository "github.com/faridlan/firestore-go/repository/skill_repository"
 	"github.com/stretchr/testify/assert"
 )
@@ -22,4 +23,31 @@ func TestSkillFind(t *testing.T) {
 	assert.Nil(t, err)
 
 	fmt.Println(skillResponse)
+}
+
+func TestSkillFindId(t *testing.T) {
+
+	skillResponse, err := skillRepo.FindId(ctx, client, "3nBpOEmlcIJVFEbQNS00")
+	assert.Nil(t, err)
+
+	fmt.Println(skillResponse)
+}
+
+func TestSkillCreate(t *testing.T) {
+
+	skill := &domain.Skill{
+		Name: "skill-unit-tes",
+	}
+
+	skillResponse, err := skillRepo.Save(ctx, client, skill)
+	assert.Nil(t, err)
+
+	fmt.Println(skillResponse)
+}
+
+func TestSkillUpdate(t *testing.T) {
+
+	err := skillRepo.Delete(ctx, client, "3nBpOEmlcIJVFEbQNS00")
+	assert.Nil(t, err)
+
 }
