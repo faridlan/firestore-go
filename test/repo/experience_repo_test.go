0297eec0a6f273e83ddedab1ff245b40221aca4f
Faridlan Nul Hakim <faridlannulhakim@gmail.com>
2024-07-14 23:47:03 +0700

    [ADD] experience test

---
+++ test/repo/experience_repo_test.go
@@ -0,0 +1,13 @@
+package repo
+
+import (
+	"testing"
+
+	experiencerepository "github.com/faridlan/firestore-go/repository/experience_repository"
+)
+
+var experienceRepo experiencerepository.ExperienceRepository
+
+func TestFindAllExperience(t *testing.T) {
+
+}
