022859af527d92b98039ddfbb3c20afd33f12d74
Faridlan Nul Hakim <faridlannulhakim@gmail.com>
2024-07-15 23:02:16 +0700

    [ADD] experience test

--- test/repo/experience_repo_test.go
+++ test/repo/experience_repo_test.go
@@ -11,3 +11,19 @@ var experienceRepo experiencerepository.ExperienceRepository
 func TestFindAllExperience(t *testing.T) {
 
 }
+
+func TestFindByIdExperience(t *testing.T) {
+
+}
+
+func TestSaveExperience(t *testing.T) {
+
+}
+
+func TestDeleteExperience(t *testing.T) {
+
+}
+
+func TestUpdateExperience(t *testing.T) {
+
+}
